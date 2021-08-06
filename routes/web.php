c427f33786e0631fa6aeee955cfe0cc73d64156c
candlelove <77607022@qq.com>
2021-08-06 13:04:49 +0800

    用户显示 页面

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 //用户注册
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', UsersController::class);
